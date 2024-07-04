d0400423be83b97d5ab9d394aff72fd630f5c148
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-07-04 20:56:55 -0300

    feat: update users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,7 @@
 'use client'
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Pencil1Icon, TrashIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import UsersTable from './components/users-table';
 
 export default function Users() {
   const users = [
@@ -21,10 +14,6 @@ export default function Users() {
     { name: "Gustavo Santos", email: "gustavo.santos@example.com", cpf: "147.258.369-66" },
     { name: "Helena Costa", email: "helena.costa@example.com", cpf: "258.369.147-77" },
   ];
-
-  function handleClick(debug: string) {
-    alert(debug)
-  }
 
   return (
     <main className="w-full min-h-screen bg-dark-primary">
@@ -42,39 +31,7 @@ export default function Users() {
           />
         </div>
         <div className="border border-gray-700 rounded-md">
-          <Table className="bg-[#313131] rounded-md bg-clip-padding backdrop-filter bg-opacity-10 backdrop-blur-sm">
-            <TableHeader className="border-gray-600">
-              <TableRow className="border-gray-600">
-                <TableHead className="text-gray-500 py-4">Nome</TableHead>
-                <TableHead className="text-gray-500 py-4">E-mail</TableHead>
-                <TableHead className="text-gray-500 py-4">CPF</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody className="text-gray-300">
-              {users.map((user, index) => (
-                <TableRow key={index} className="border-0">
-                  <TableCell>{user.name}</TableCell>
-                  <TableCell>{user.email}</TableCell>
-                  <TableCell>{user.cpf}</TableCell>
-                  <TableCell className="flex justify-center">
-                    <div className="flex gap-x-2">
-                      <div
-                        className="flex py-1 px-2 gap-x-1 rounded-md hover:cursor-pointer bg-blue-500"
-                        onClick={() => alert(user.name)}
-                      >
-                        <Pencil1Icon className="w-5 h-5 text-gray-100" />
-                        <p>Editar</p>
-                      </div>
-                      <div className="flex py-1 px-2 rounded-md hover:cursor-pointer bg-red-500">
-                        <TrashIcon className="w-5 h-5 text-gray-100" />
-                        <p>Excluir</p>
-                      </div>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <UsersTable data={users} />
         </div>
       </div>
     </main>
