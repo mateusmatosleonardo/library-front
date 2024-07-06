d6777f88edcb76114709e7aae2b59a3a61dc7cfc
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-07-05 23:54:41 -0300

    feat: add header in page book

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,7 +1,12 @@
 export default function Books() {
   return (
-    <main className="flex flex-1 w-full min-h-screen bg-gray-100">
-
+    <main className="flex flex-1 w-full min-h-screen bg-[#F2F2F2]">
+      <div className="mx-12 mt-12">
+        <h1 className="text-2xl font-bold text-slate-900">
+          Livros
+        </h1>
+      </div>
+      <div className="flex flex-col gap-y-6 px-12 pt-6"></div>
     </main>
   )
 }
