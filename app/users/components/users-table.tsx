287155b9ab6e746663fd98ff3edcce8f78461c51
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-07-31 13:09:01 -0300

    💄 Update table user UI

--- app/users/components/users-table.tsx
+++ app/users/components/users-table.tsx
@@ -7,8 +7,24 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
-import { Pencil1Icon, TrashIcon } from '@radix-ui/react-icons'
+} from "@/components/ui/table"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage
+} from "@/components/ui/avatar"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { formatToCPF, formatToPhone } from "brazilian-values"
 
 type UsersTableProps = {
   data?: any[]
@@ -19,30 +35,66 @@ export default function UsersTable({ data }: UsersTableProps) {
     <Table className="bg-white rounded-md">
       <TableHeader className="border-gray-200">
         <TableRow className="border-gray-200">
+          <TableHead className="text-slate-950 py-4"></TableHead>
           <TableHead className="text-slate-950 py-4">Nome</TableHead>
           <TableHead className="text-slate-950 py-4">E-mail</TableHead>
           <TableHead className="text-slate-950 py-4">CPF</TableHead>
+          <TableHead className="text-slate-950 py-4">Telefone</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody className="text-slate-900">
         {data?.map((user, index) => (
           <TableRow key={index} className="border-0">
+            <TableCell>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>Avatar</AvatarFallback>
+              </Avatar>
+            </TableCell>
             <TableCell>{user.name}</TableCell>
             <TableCell>{user.email}</TableCell>
-            <TableCell>{user.cpf}</TableCell>
+            <TableCell>
+              {formatToCPF(user.cpf)}
+            </TableCell>
+            <TableCell>
+              {formatToPhone(user.phone)}
+            </TableCell>
             <TableCell className="flex justify-center">
               <div className="flex gap-x-2">
-                <div
-                  className="flex py-1 px-2 gap-x-1 rounded-md hover:cursor-pointer bg-blue-500"
-                  onClick={() => alert(user.name)}
-                >
-                  <Pencil1Icon className="w-5 h-5 text-gray-200" />
-                  <p className="text-gray-200">Editar</p>
-                </div>
-                <div className="flex py-1 px-2 rounded-md hover:cursor-pointer bg-red-500">
-                  <TrashIcon className="w-5 h-5 text-gray-200" />
-                  <p className="text-gray-200">Excluir</p>
-                </div>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button>
+                      Editar
+                    </Button>
+                  </DialogTrigger>
+                </Dialog>
+
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button variant="destructive">
+                      Excluir
+                    </Button>
+                  </DialogTrigger>
+
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Excluir usuário</DialogTitle>
+                      <DialogDescription>Tem certeza que deseja excluir este usuário?</DialogDescription>
+                      <p>{user.name}</p>
+                    </DialogHeader>
+
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant="ghost">
+                          Cancelar
+                        </Button>
+                      </DialogClose>
+                      <Button variant="destructive">
+                        Excluir
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
               </div>
             </TableCell>
           </TableRow>
