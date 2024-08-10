1768e178eabcd4904737e84e3e85f605427d6f87
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-08-09 22:52:24 -0300

    update side bar style

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,19 +1,18 @@
-
 "use client"
 
 import { PersonIcon, BookmarkIcon } from '@radix-ui/react-icons'
 import Image from 'next/image'
 import NavLink from './nav-link'
-import Icon from '@/public/livro.png'
+import Icon from '@/public/open-book.png'
 import { usePathname } from 'next/navigation'
 
 export default function SideBar() {
   const pathname = usePathname()
 
   return (
-    <div className="flex flex-col w-[18rem] min-h-screen p-6 bg-zinc-900">
+    <div className="flex flex-col w-[18rem] min-h-screen p-6 bg-gray-100">
       <div className='flex items-center gap-x-4 w-7 h-7'>
-        <h1 className='text-3xl font-bold text-gray-300'>
+        <h1 className='text-3xl font-bold text-zinc-800'>
           Biblioteca
         </h1>
         <Image alt='icon' src={Icon} />
@@ -21,17 +20,17 @@ export default function SideBar() {
       <div className='flex flex-col pt-6 gap-y-2'>
         <NavLink
           href='/users'
-          bg={pathname === '/users' ? 'bg-green-600' : 'bg-transparent'}
+          bg={pathname === '/users' ? 'bg-gray-200' : 'bg-transparent'}
         >
-          <PersonIcon className='w-3 h-3 text-white' />
-          <p className='text-gray-300'>Usuários</p>
+          <PersonIcon className='w-3.5 h-3.5 text-zinc-800' />
+          <p className='text-zinc-800 font-medium'>Usuários</p>
         </NavLink>
         <NavLink
           href='/books'
-          bg={pathname === '/books' ? 'bg-green-600' : 'bg-transparent'}
+          bg={pathname === '/books' ? 'bg-gray-200' : 'bg-transparent'}
         >
-          <BookmarkIcon className='w-3 h-3 text-white' />
-          <p className='text-gray-300'>Livros</p>
+          <BookmarkIcon className='w-3.5 h-3.5 text-zinc-800' />
+          <p className='text-zinc-800 font-medium'>Livros</p>
         </NavLink>
       </div>
     </div>
