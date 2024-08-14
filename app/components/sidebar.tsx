71d4b00fe415c1a9ed24785fa7d6ef246e24db55
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-08-13 23:17:31 -0300

    update side bar style

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,6 +1,12 @@
 "use client"
 
 import { PersonIcon, BookmarkIcon } from '@radix-ui/react-icons'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import Image from 'next/image'
 import NavLink from './nav-link'
 import Icon from '@/public/open-book.png'
@@ -10,28 +16,42 @@ export default function SideBar() {
   const pathname = usePathname()
 
   return (
-    <div className="flex flex-col w-[18rem] min-h-screen p-6 bg-gray-100">
+    <div className="flex flex-col min-h-screen p-6 bg-gray-100">
       <div className='flex items-center gap-x-4 w-7 h-7'>
-        <h1 className='text-3xl font-bold text-zinc-800'>
-          Biblioteca
-        </h1>
         <Image alt='icon' src={Icon} />
       </div>
-      <div className='flex flex-col pt-6 gap-y-2'>
-        <NavLink
-          href='/users'
-          bg={pathname === '/users' ? 'bg-gray-200' : 'bg-transparent'}
-        >
-          <PersonIcon className='w-3.5 h-3.5 text-zinc-800' />
-          <p className='text-zinc-800 font-medium'>Usuários</p>
-        </NavLink>
-        <NavLink
-          href='/books'
-          bg={pathname === '/books' ? 'bg-gray-200' : 'bg-transparent'}
-        >
-          <BookmarkIcon className='w-3.5 h-3.5 text-zinc-800' />
-          <p className='text-zinc-800 font-medium'>Livros</p>
-        </NavLink>
+      <div className='flex flex-col items-center pt-6 gap-y-2'>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                href='/users'
+                bg={pathname === '/users' ? 'bg-gray-200' : 'bg-transparent'}
+              >
+                <PersonIcon className='w-[1.1rem] h-[1.1rem] text-zinc-800' />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side='right'>
+              <p>Usuários</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <NavLink
+                href='/books'
+                bg={pathname === '/books' ? 'bg-gray-200' : 'bg-transparent'}
+              >
+                <BookmarkIcon className='w-[1.1rem] h-[1.1rem] text-zinc-800' />
+              </NavLink>
+            </TooltipTrigger>
+            <TooltipContent side='right'>
+              <p>Livros</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   )
