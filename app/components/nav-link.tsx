5d2791c6afcecb6e6b4b6f9da0f4cdf902741bbb
mateusmatosleonardo <m4teusmatos@gmail.com>
2024-08-28 23:20:47 -0300

    refactor pages and components in dashboard

--- app/components/nav-link.tsx
+++ app/components/nav-link.tsx
@@ -12,7 +12,7 @@ export default function NavLink(props: NavLinkProps) {
     <Link
       href={props.href}
     >
-      <div className={`flex items-center gap-x-2 py-2.5 px-2 mx-4 rounded-md duration-300 hover:cursor-pointer hover:bg-orange-primary ${props.current_page === props.href ? 'bg-orange-primary' : ''}`}>
+      <div className={`flex items-center gap-x-2 py-2.5 px-2 mx-4 rounded-lg duration-300 hover:cursor-pointer hover:bg-orange-primary ${props.current_page === props.href ? 'bg-orange-primary' : ''}`}>
         {props.children}
       </div>
     </Link>
